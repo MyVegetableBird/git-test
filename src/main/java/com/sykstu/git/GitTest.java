828fd08aee86c2b124cb344f26ebc68957d082ac
Maven <Maven@qq.com>
2022-08-19 16:14:17 +0800

    first commit

--- src/main/java/com/sykstu/git/GitTest.java
+++ src/main/java/com/sykstu/git/GitTest.java
@@ -3,6 +3,6 @@ package com.sykstu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
-        System.out.println("dsadad");
+        System.out.println("ddadad");
     }
 }
